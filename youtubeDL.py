d70ec654dfe4eb086a96e1fa44438505a6934649
Renzo Westerbeek <renzowesterbeek@gmail.com>
2014-07-23 09:33:52 +0200

    Add pafy.py file & download prefers m4a
    Instead of downloading the best audio (ogg), program now downloads the
    iTunes-compatible m4a if it’s present.

--- youtubeDL.py
+++ youtubeDL.py
@@ -1,16 +1,26 @@
 import pafy
 
-# Gets all the urls from the musicfile
-musicFile = open("musicFile.txt", "r")
-downloadList = []
-for url in musicFile:
-    downloadList.append(url.strip())
-musicFile.close()
+def getDownloadList():
+	# Gets all the urls from the musicfile
+	musicFile = open("musicFile.txt", "r")
+	downloadList = []
+	for url in musicFile:
+		downloadList.append(url.strip())
+	musicFile.close()
+	return downloadList
+
+downloadList = getDownloadList()
 
 # Downloads music
 for url in downloadList:
 	audio = pafy.new(url)
-	audiofile = audio.getbestaudio()
-	myfilename = "audiofiles/" + audiofile.title + "." + audiofile.extension
+	audiostreams = audio.audiostreams
+	audiofiles = []
+	for a in audiostreams:
+		audiofiles.append(a.extension)
+	print(audiofiles)
+
+	audiofile = audio.getbestaudio(preftype="m4a")
+	myfilename = audiofile.title + "." + audiofile.extension
 
 	audiofile.download(filepath=myfilename)
